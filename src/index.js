329b6284c6182da6d9715abc37c9825c42c68927
tushar <tyagi121201@gmail.com>
2021-07-30 18:45:08 +0530

    message

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import _ from "lodash";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
+const API_KEY = "AIzaSyCHt_a3zMoOhir2ysiStE1j-UER7ASCWkE";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch("asphalt");
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
+
+    return (
+      <div> 
+        <div className ="logo">
+        <div className="icon">
+       <i className="fab fa-youtube" ></i>
+        </div>
+        <div>
+          <div className="title">Utube</div>
+        </div>
+        </div>
+        <div>
+
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+          />
+          </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
