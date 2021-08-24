345be066956f7e9b4c31fd166b8f582fc1a7fc2a
tushrtyagi <78441124+tushrtyagi@users.noreply.github.com>
2021-08-24 12:23:38 +0530

    Update video_list_item.js

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -10,7 +10,7 @@ const VideoListItem = ({ video, onVideoSelect }) => {
           <img className="media-object" src={imageUrl} />
         </div>
         <div className="media-body">
-          <div className="media-heading desc">{video.snippet.title}</div>
+          <div className="media-heading">{video.snippet.title}</div>
         </div>
       </div>
     </li>
